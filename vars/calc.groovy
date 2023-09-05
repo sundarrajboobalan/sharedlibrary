404862f8fc345c0c4e0040cbcc4d57bae7589fdf
sundarrajboobalan <73453612+sundarrajboobalan@users.noreply.github.com>
2023-09-05 15:57:28 +0530

    Update calc.groovy

--- vars/calc.groovy
+++ vars/calc.groovy
@@ -1,9 +1,9 @@
 def add(a,b)
 {
-  echo "addition of ${a} and ${b} is ${a+b)"
+  echo "addition of ${a} and ${b} is ${a+b}"
 }
   def sub(a,b)
 {
-  echo "addition of ${a} and ${b} is ${a-b)"
+  echo "addition of ${a} and ${b} is ${a-b}"
 }
   
